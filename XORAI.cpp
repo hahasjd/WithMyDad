f5488beee1128e729dab9a909ba3f63dd57b3e42
Andrey Burlakov <andrey.burlakov@vmssoftware.com>
2023-10-23 18:40:07 +0200

    Исправления в исходном файле. Исправления в настройках.

--- XORAI.cpp
+++ XORAI.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 float input_layer[2];
-float hidden_layer[2, 2];
+float hidden_layer[2][2];
 float output_layer[1];
-float weights_layer_1[2, 2];
-float weights_layer_2[2, 2];
+float weights_layer_1[2][2];
+float weights_layer_2[2][2];
 float weights_output[2];
-float init(a)
+void init(void)
 {
     for(int i = 0; i < 2; i++)
     {
-        for(int j = 0; i < 2; j++)
+        for(int j = 0; j < 2; j++)
         {
-            weights_layer_1[i, j] = 0.5;
-            weights_layer_2[i, j] = 0.5;
+            weights_layer_1[i][j] = 0.5;
+            weights_layer_2[i][j] = 0.5;
         }
         weights_output[i] = 0.5;
     }
@@ -23,8 +23,10 @@ float init(a)
 
 int main()
 {
-    for(int i = 0; i<=2; i++)
+    init();
+    for(int i = 0; i < 2; i++)
     {
         cout << "Hello !";
     }
+    return 0;
 }
